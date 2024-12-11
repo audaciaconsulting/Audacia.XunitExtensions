574a4e1c91cbb297a8436ad734b1c33417548e1d
tom.shephard <tom.shephard@audacia.co.uk>
2024-12-11 12:25:24 +0000

    test

--- tests/Audacia.XunitExtensions.Tests/Integration/Inconclusive/InconclusiveTests.cs
+++ tests/Audacia.XunitExtensions.Tests/Integration/Inconclusive/InconclusiveTests.cs
@@ -5,15 +5,7 @@ namespace Audacia.XunitExtensions.Tests.Integration.Inconclusive
 {
     public class InconclusiveTests
     {
-        [InconclusiveFact]
-        public void Fact_Test_Marked_As_Skipped_If_Inconclusive_Test_Exception_Thrown()
-        {
-            throw new InconclusiveTestResultException("Inconclusive test message.");
-        }
 
-        [InconclusiveTheory]
-        [InlineData(1)]
-        public void Theory_Test_Marked_As_Skipped_If_Inconclusive_Test_Exception_Thrown(int i)
         {
             throw new InconclusiveTestResultException($"Inconclusive test message with parameter {i}.");
         }
